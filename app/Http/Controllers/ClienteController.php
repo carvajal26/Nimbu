166f796a5754609f358cc0c0a43e9eb882793ddb
Nayeli Barrantes <nayeli.carvajal.barrantes@est.una.ac.cr>
2022-08-23 18:41:00 -0600

    primero cambios

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('Client.index');
+    }
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $data = array(
+            'count_user' => DB::table('users')->count(),
+            'menu'      => 'admin.menu.v_menu_admin',
+            'content' => 'admin.content.view_dashboard'
+        );
+        return view('admin.layouts.v_template',$data);
+    }
+}
